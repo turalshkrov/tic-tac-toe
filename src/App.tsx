ed1e9d79705d6bbf1b5ba2fdee795e3390d0a73f
tural.shkrov <tural.shkrov@gmail.com>
2023-11-21 16:09:06 +0400

    Setup: Splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import Header from './components/header/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 import { ThemeProvider } from './components/theme/themeProvider';
+import SplashScreen from './pages/splash screen/SplashScreen';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Settings from './pages/settings/Settings';
 
 function App() {
   return (
-      <div className='App'>
-        <ThemeProvider>
-          <Header />
-        </ThemeProvider>
-      </div>
+      <ThemeProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<SplashScreen/>}/>
+            <Route path='/settings' element={<Settings/>}/>
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
   )
 }
 
---
+++ src/pages/splash screen/SplashScreen.tsx
@@ -0,0 +1,22 @@
+import { Container } from "react-bootstrap"
+import { Link } from "react-router-dom"
+
+export default function SplashScreen() {
+  return (
+    <Container>
+      <div className="d-flex justify-content-center align-items-center h-100">
+        <div className="start-btn-container">
+          <Link to='/settings'>
+            <div className="start-btn d-flex flex-column align-items-center justify-content-center">
+              <span className='fw-bold'>tic-</span>
+              <span className='fw-bold'>tac-</span>
+              <span className='fw-bold'>toe.</span>
+            </div>
+            <div className="start-btn-shadow">
+            </div>
+          </Link>
+        </div>
+      </div>
+    </Container>
+  )
+}
